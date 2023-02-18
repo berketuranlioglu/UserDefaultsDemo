a89b0880243ee448d64ba49c2dae3358258600ef
Berke Turanlıoğlu <berke.turanlioglu@gmail.com>
2023-02-18 14:20:46 +0300

    Initial Commit

---
+++ UserDefaultsDemo/UserDefaultsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsDemoApp.swift
+//  UserDefaultsDemo
+//
+//  Created by Berke Turanlıoğlu on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
